e10e4f53101e2a0ddc221ed3b1c7aa359ed88851
segin-GH <segin.bytes@gmail.com>
2023-03-25 01:30:25 +0530

    .

--- halfAdder/halfAdder_tb.v
+++ halfAdder/halfAdder_tb.v
@@ -10,6 +10,8 @@ module half_adder_tb();
     initial begin
         $dumpfile("half_adder_tb.vcd");
         $dumpvars(0, half_adder_tb);
+
+        $monitor (" S = %b, C = %b, A = %b, B = %b \n", s, c, a, b);
             a = 0; b = 0; #1;
             a = 1; b = 0; #1;
             a = 0; b = 1; #1;
